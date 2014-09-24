e005f20f06d17a583e56542967dde7691c740e82
hiyuki2578 <tomoka319@gmail.com>
2014-09-24 16:49:23 +0900

    fix error

--- src/net/hiyuki2578/Web_browser/MainActivity.java
+++ src/net/hiyuki2578/Web_browser/MainActivity.java
@@ -113,7 +113,7 @@ public class MainActivity extends Activity {
 		//          ＞＞  ＼／
 		boolean checkboxValue = spf.getBoolean("EnableJavaScripts", false);
 		String userAgentString = myWebView.getSettings().getUserAgentString();
-		myWebView.getSettings().setUserAgentString(userAgentString+" hiyuki_browser");  
+		myWebView.getSettings().setUserAgentString(userAgentString+" hiyuki2578");  
 		userAgentString = myWebView.getSettings().getUserAgentString();
 		if(checkboxValue == true){
 			webSettings.setJavaScriptEnabled(true);
